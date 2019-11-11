d383331f212caa04bf0474c72ba836128bc22409
Fran Borg <francesca.bte@outlook.com>
2019-11-11 10:32:43 +0100

    Add boilerplate

---
+++ index.js
@@ -0,0 +1,12 @@
+//  Add your answers here
+
+
+
+
+
+
+
+
+
+// DO NOT edit below this line - editing will result in an AUTOMATIC FAIL
+module.exports = {nameOfCity, isDivisible, missingAngle, rainingToday, geometricalSequence, multiplesOfThree, powerOf, vowelCount};
